d776ed01be40449839392eb6da45e9af3d40a72d
Laszlo Vigh <laszlo.vigh@ibm.com>
2022-08-16 17:11:48 +0200

    lock to portrait and persist pin setting

--- app/src/main/java/com/dzac/childmath/MainActivity.java
+++ app/src/main/java/com/dzac/childmath/MainActivity.java
@@ -1,11 +1,15 @@
 package com.dzac.childmath;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.LruCacheKt;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button btnPlusMinus = (Button)findViewById(R.id.buttonPlusMinus);
         Button btnLesserGreater = (Button)findViewById(R.id.buttonLesserGreater);
+
         btnPlusMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,7 +38,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
 
+        if (!sharedPref.contains("autopin")) {
+            editor.putBoolean("autopin", true);
+            editor.commit();
+        }
+        Boolean autopin = sharedPref.getBoolean("autopin", true);
+
+        CheckBox autopin_cbox = (CheckBox) findViewById(R.id.autoPin);
+        autopin_cbox.setChecked(autopin);
+
+        autopin_cbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putBoolean("autopin", autopin_cbox.isChecked());
+                editor.commit();
+            }
+        });
+
+        if (autopin_cbox.isChecked()) {
+            startLockTask();
+        }
     }
 
 }
--- app/src/main/java/com/dzac/childmath/PlusMinus.java
+++ app/src/main/java/com/dzac/childmath/PlusMinus.java
@@ -60,6 +60,7 @@ public class PlusMinus extends AppCompatActivity {
 
 
 
+
     }
 
     @Override
